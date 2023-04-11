80c9606bf2bc1cbe3b510655ff88a0024651dea6
yazmusleh1 <32080833+yazmusleh1@users.noreply.github.com>
2023-04-11 13:11:47 -0500

    Added resume

--- src/components/Timeline/timeline.tsx
+++ src/components/Timeline/timeline.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import timeline from '../../data/timeline';
-import TimelineEvent from '../TimelineEvent/timelineevent';
-import Title from '../Title/title';
+import React from "react";
+import timeline from "../../data/timeline";
+import TimelineEvent from "../TimelineEvent/timelineevent";
+import Title from "../Title/title";
 
 const Timeline: React.FC = () => {
   return (
     <div className="max-w-3xl mx-auto  ">
       <div className="relative p-6">
-      <Title>Timeline</Title>
+        <div className="flex w-full justify-between ">
+          <Title>Timeline</Title>
+          <a
+            href="./yazanmuslehresume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="self-start font-semibold"
+          >
+            View Resume &#x2197;
+          </a>
+        </div>
+
         {timeline.map((event) => (
           <>
-            <TimelineEvent date={event.date} role={event.role} duration={event.duration} description={event.description} />
+            <TimelineEvent
+              date={event.date}
+              role={event.role}
+              duration={event.duration}
+              description={event.description}
+            />
           </>
         ))}
       </div>
